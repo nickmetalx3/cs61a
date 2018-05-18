824fc11d1e07f72a6f405ae8775f17fe37ca95d2
nickmetalx3 <35188625+nickmetalx3@users.noreply.github.com>
2018-05-18 11:00:25 +0400

    lab 0 completed

---
+++ labs/lab00/lab00.py
@@ -0,0 +1,10 @@
+def twenty_seventeen():
+    """Come up with the most creative expression that evaluates to 2016,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_sixteen()
+    2016
+    """
+    return int(524 * 4 + (10 - 89))
+
+#print(twenty_seventeen())   quick test
