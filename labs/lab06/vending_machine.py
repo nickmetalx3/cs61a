cd4c502625a1c58135b6cd76c89fa1079cdfcc48
nickmetalx3 <35188625+nickmetalx3@users.noreply.github.com>
2018-05-19 19:22:05 +0400

    lab 6 completed

---
+++ labs/lab06/vending_machine.py
@@ -0,0 +1,25 @@
+def vending_machine(snacks):
+    """Cycles through list of snacks.
+
+    >>> vender = vending_machine(('chips', 'chocolate', 'popcorn'))
+    >>> vender()
+    'chips'
+    >>> vender()
+    'chocolate'
+    >>> vender()
+    'popcorn'
+    >>> vender()
+    'chips'
+    >>> other = vending_machine(('brownie',))
+    >>> other()
+    'brownie'
+    >>> vender()
+    'chocolate'
+    """
+    index = 0
+    def vender():
+        nonlocal index
+        print (repr(snacks[index % len(snacks)]))
+        index += 1
+
+    return vender
